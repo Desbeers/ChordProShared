ab9ea124c9e15d4dfa2b9299ad12b7e0066de1bc
Nick Berendsen <58348935+Desbeers@users.noreply.github.com>
2024-06-30 06:08:12 +0200

    Snapshot

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -20,11 +20,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "ChordProShared",
-            dependencies: [],
-            swiftSettings: [
-                .enableExperimentalFeature("StrictConcurrency")
-            ]
+            name: "ChordProShared"
         ),
     ]
 )
